031a85498459f14f79e1de942cc729b185be0e6e
yujinaa <olim920@naver.com>
2022-05-13 17:30:31 +0900

    할일추가시  목록이 늘어나는 기능

--- Todolist/list.js
+++ Todolist/list.js
@@ -1,21 +1,20 @@
-
-//버튼 변수지정
-const btn = document.querySelector(".inputField button");
-
-
+const btn = document.getElementById('btn');
+let addValue = document.getElementById('addValue');
+let result = document.getElementById('result');
 //function 문법
-function addTodo(){   //+버튼을 누르면
-    addValue = document.getElementById('addValue').value;
-    document.getElementById("result").innerText = addValue;
+
+function addTodo(){
+    let list = document.createElement("li");
+    list.innerHTML = addValue.value;
+    result.appendChild(list);
 }
 
 
+//입력값 한개만 나오는 코드
 // function addTodo(){   //+버튼을 누르면
     // const addValue = document.getElementById('addValue').value;
     // document.getElementById("result").innerText = addValue;
-    
 // }
-
 
 
 //변수지정 방법 : const, let, var
