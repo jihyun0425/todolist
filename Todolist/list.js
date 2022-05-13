730652862b9ba1a28fad3a63fc5251c05b1f9084
yujinaa <olim920@naver.com>
2022-05-14 05:01:24 +0900

    할일추가시 삭제버튼 추가돼는 2번째 방법코드

--- Todolist/list.js
+++ Todolist/list.js
@@ -3,16 +3,15 @@ let addValue = document.getElementById('addValue'); //할일 입력
 let result = document.getElementById('result');// 추가된 할일
 
 
-
 function addTodo(){
     let list = document.createElement("li");
+    let del = document.createElement('button');
     list.innerHTML = addValue.value;
     result.appendChild(list);
+    list.appendChild(del);
     /*할일 추가할 때마다 삭제버튼도 같이 추가돼기 */
-    list.innerHTML+="<button type='button' id ='remove' onclick='removeList()'><i class='xi-trash xi-2x'></span></i></button>";
+    // list.innerHTML+="<button type='button' id ='remove' onclick='removeList()'><i class='xi-trash xi-2x'></span></i></button>";
     addValue.value = ""; //할일 입력창 초기화
-
-    // <button type="button" id = "removebtn" onclick="remove()"><i class="xi-trash xi-2x"></i></button>;
     //할일 완료 후 클릭시 밑줄 표시
     list.addEventListener("click", function(){
         list.style.textDecoration = "line-through";
