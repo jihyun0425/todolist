3b09ceab32376da6444def10d15c4fb5c4864e40
yujinaa <olim920@naver.com>
2022-05-15 02:39:09 +0900

    코드수정

--- Todolist/list.js
+++ Todolist/list.js
@@ -26,29 +26,20 @@ function addTodo(){
     /*할일 추가할 때마다 삭제버튼도 같이 추가돼기-이 방법도 가능 */
     //list.innerHTML+="<button type='button' id ='remove' onclick='removeList()'><i class='xi-trash xi-2x'></span></i></button>";
     
-    addValue.value = "";       //할일 입력창 초기화
-    addValue.focus();           //강제 커서 깜빡임
-    list.addEventListener("click", function(){  //할일 완료 후 클릭시 밑줄로 표시
+    addValue.value = "";                            //할일 입력창 초기화
+    addValue.focus();                               //강제 커서 깜빡임
+    list.addEventListener("click", function(){      //할일 완료 후 클릭시 밑줄로 표시
         list.style.textDecoration = "line-through";
-        list.style.color = "gray";  //클릭시 색변환
+        list.style.color = "gray";                  //클릭시 색변환
     })
 }
 }
-
-//할일 삭제시
+//할일 목록 삭제시
 function deleteList(e){ //삭제 버튼(x) 클릭시 
     let removeOne = e.target.parentElement;  //선택한 목록 한개만 지우기(부모 객체를 지운다)
     removeOne.remove();
 }
-
 // //모두 삭제시
-// function allClearList(e){
-//     if(confirm("정말 삭제하시겠습니까?")==true){ //취소메시지가 true면
-//         result.innerText='';                    //전체 삭제
-//     }else{
-//         return false;                           //취소
-//     }
-// }
 function allClearList(e){
     if(confirm("정말 삭제하시겠습니까?")==true){ //취소메시지가 true(ok)일때
         if(result.innerText==''){                      //목록칸이 비어있다면
@@ -65,7 +56,6 @@ function allClearList(e){
     // const addValue = document.getElementById('addValue').value;
     // document.getElementById("result").innerText = addValue;
 // }
-
 
 //변수지정 방법 : const, let, var
 //css에는 꾸미는 기능만, js에서 동적으로 처리하는 기능만 넣은 상태
